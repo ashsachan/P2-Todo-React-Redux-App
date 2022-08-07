32d90f6906295300115175a1def3f11128b34185
ashsachan <ashishsachan0712@outlook.in>
2022-08-07 12:25:07 +0530

    Commit 05: Complete is changing state.

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -7,7 +7,7 @@ const addTodoReducer = createSlice({
   initialState,
   reducers: {
     //here we will write our reducer
-
+    
     //Adding todos
     addTodos: (state, action) => {
       state.push(action.payload);
@@ -16,24 +16,41 @@ const addTodoReducer = createSlice({
 
     //remove todos
     removeTodos: (state, action) => {
-        return state.filter((item) => item.id !== action.payload);
-      },
-    
+      return state.filter((item) => item.id !== action.payload);
+    },
+
     //update todos
     updateTodos: (state, action) => {
-        return state.map((todo) => {
-          if (todo.id === action.payload.id) {
-            return {
-              ...todo,
-              item: action.payload.item,
-            };
-          }
-          return todo;
-        });
-      },
+      return state.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return {
+            ...todo,
+            item: action.payload.item,
+          };
+        }
+        return todo;
+      });
+    },
 
-    }
-})
+    //completed
+    completeTodos: (state, action) => {
+      return state.map((todo) => {
+        if (todo.id === action.payload) {
+          return {
+            ...todo,
+            completed: true,
+          };
+        }
+        return todo;
+      });
+    },
+  },
+});
 
-export const { addTodos, removeTodos, updateTodos,} = addTodoReducer.actions;
+export const {
+  addTodos,
+  removeTodos,
+  updateTodos,
+  completeTodos,
+} = addTodoReducer.actions;
 export const reducer = addTodoReducer.reducer;
