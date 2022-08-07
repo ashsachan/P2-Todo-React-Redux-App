6afbffa0ee81230a8c6310450c6c213015606e88
ashsachan <ashishsachan0712@outlook.in>
2022-08-07 12:07:01 +0530

    Commit 04: Delete Item Working with Redux States.

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
-import { addTodos } from "../redux/reducer";
+import { addTodos, removeTodos } from "../redux/reducer";
 
 const mapStateToProps = (state) => {
   return {
@@ -11,12 +11,13 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     addTodo: (obj) => dispatch(addTodos(obj)),
+    removeTodo: (id) => dispatch(removeTodos(id)),
   };
 };
 
 const Todos = (props) => {
   const [todo, setTodo] = useState("");
-  
+
   const handleChange = (e) => {
     setTodo(e.target.value);
   };
@@ -47,7 +48,15 @@ const Todos = (props) => {
 
       <ui>
         {props.todos.map((item) => {
-          return <li key={item.id}> {item.item}</li>;
+          return (
+            <li key={item.id}>
+              {item.item}
+              <button onClick={() => props.removeTodo(item.id)}>
+                {" "}
+                Delete{" "}
+              </button>
+            </li>
+          );
         })}
       </ui>
     </div>
