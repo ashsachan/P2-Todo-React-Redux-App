781e8006414d720bb026ebdec156a3d8ca66b624
ashsachan <ashishsachan0712@outlook.in>
2022-08-07 11:41:52 +0530

    Commit 01: Components are Rendering..

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import "./css/main.css";
+import DisplayTodos from "./components/DisplayTodos";
+import Todos from "./components/Todos";
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <h1>Todo App Using React and Redux</h1>
+      <Todos />
+      <DisplayTodos />
     </div>
   );
 }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,8 +1,10 @@
 import React from "react";
+import TodoItem from "./TodoItem";
 
 const Todos = () => {
   return <div>
-    
+      I am Inside Todos.
+      <TodoItem/>
   </div>;
 };
 
